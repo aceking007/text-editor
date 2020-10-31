78dd1b85dd09b4671d4328239a31ef40c3e08ce9
Arpit Omprakash <omprakasharpit@gmail.com>
2020-10-31 18:57:34 +0530

    added welcome message

--- kilo.c
+++ kilo.c
@@ -11,6 +11,7 @@
 
 /*** defines ***/
 
+#define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
@@ -174,7 +175,23 @@ void editorProcessKeyPress() {
 void editorDrawRows(struct abuf *ab) {
 	int y;
 	for (y = 0; y < E.screenrows; y++) {
-		abAppend(ab, "~", 1);
+		// display the welcome message
+		if (y == E.screenrows / 3) {
+			char welcome[80];
+			int welcomelen = snprintf(welcome, sizeof(welcome),
+					"Kilo editor -- version %s", KILO_VERSION);
+			if (welcomelen > E.screencols) welcomelen = E.screencols;
+			// centering the message
+			int padding = (E.screencols - welcomelen) / 2;
+			if (padding) {
+				abAppend(ab, "~", 1);
+				padding--;
+			}
+			while (padding--) abAppend(ab, " ", 1);
+			abAppend(ab, welcome, welcomelen);
+		} else {
+			abAppend(ab, "~", 1);
+		}
 
 		// clears the line
 		abAppend(ab, "\x1b[K", 3);
