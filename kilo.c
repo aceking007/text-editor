addfa1134f3bff23aa1804d632dbe59ec7d3f2a5
Arpit Omprakash <omprakasharpit@gmail.com>
2020-11-01 16:20:25 +0530

    added horizontal scrolling

--- kilo.c
+++ kilo.c
@@ -42,6 +42,7 @@ typedef struct erow {
 struct editorConfig {
 	int cx, cy;
   int rowoff;
+  int coloff;
 	int screenrows;
 	int screencols;
   int numrows;
@@ -261,9 +262,7 @@ void editorMoveCursor(int key) {
 			}
 			break;
 		case ARROW_RIGHT:
-			if (E.cx != E.screencols - 1) {
-				E.cx++;
-			}
+			E.cx++;
 			break;
 		case ARROW_UP:
 			if (E.cy != 0) {
@@ -327,6 +326,12 @@ void editorScroll() {
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   }
+  if (E.cx < E.coloff) {
+    E.coloff = E.cx;
+  }
+  if (E.cx >= E.coloff + E.screencols) {
+    E.coloff = E.cx - E.screencols + 1;
+  }
 }
 
 // function to draw rows
@@ -353,9 +358,10 @@ void editorDrawRows(struct abuf *ab) {
   			abAppend(ab, "~", 1);
   		}
     } else {
-      int len = E.row[filerow].size;
+      int len = E.row[filerow].size - E.coloff;
+      if (len < 0) len = 0;
       if (len > E.screencols) len = E.screencols;
-      abAppend(ab, E.row[filerow].chars, len);
+      abAppend(ab, &E.row[filerow].chars[E.coloff], len);
     }
 
 		// clears the line
@@ -383,7 +389,7 @@ void editorRefreshScreen() {
 
 	// reposition cursor
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
 	abAppend(&ab, buf, strlen(buf));
 
 	// show the cursor
@@ -400,6 +406,7 @@ void initEditor() {
 	E.cx = 0;
 	E.cy = 0;
   E.rowoff = 0;
+  E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
 
