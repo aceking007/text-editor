bbdd2572bc07dd217b6bcb67bb9b959d898ae8b2
Arpit Omprakash <omprakasharpit@gmail.com>
2020-11-01 11:12:28 +0530

    fixed scroll to the top issue

--- kilo.c
+++ kilo.c
@@ -383,7 +383,7 @@ void editorRefreshScreen() {
 
 	// reposition cursor
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
 	abAppend(&ab, buf, strlen(buf));
 
 	// show the cursor
