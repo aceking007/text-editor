951c99479b74bc1feab6502fb0f86d65235600fb
Arpit Omprakash <omprakasharpit@gmail.com>
2020-11-01 10:47:33 +0530

    editor now reads a single line from file

--- kilo.c
+++ kilo.c
@@ -1,5 +1,9 @@
 /*** includes ***/
 
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -190,15 +194,25 @@ int getWindowSize(int *rows, int *cols) {
 /*** file i/o ***/
 
 // function executed when editor opens
-void editorOpen() {
-  char *line = "Hello, world!";
-  ssize_t linelen = 13;
+void editorOpen(char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp) die("fopen");
+
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  linelen = getline(&line, &linecap, fp);
+  if (linelen != -1) {
+    while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) linelen--;
 
   E.row.size = linelen;
   E.row.chars = malloc(linelen + 1);
   memcpy(E.row.chars, line, linelen);
   E.row.chars[linelen] = '\0';
   E.numrows = 1;
+  }
+  free(line);
+  fclose(fp);
 }
 
 /*** append buffer ***/
@@ -367,12 +381,13 @@ void initEditor() {
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 	enableRawMode();
 	initEditor();
-  editorOpen();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
 
-	char c;
 	while (1){
 		editorRefreshScreen();
 		editorProcessKeyPress();
